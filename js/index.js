b00ee173990850240f10fda557747e801198e381
Shubham Maurya <pnssoftwares7@gmail.com>
2019-06-23 10:34:12 +0530

    morning

--- js/index.js
+++ js/index.js
@@ -17,28 +17,28 @@ $('.news').css({
 /*---> Logo Slider <---*/
 
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-	 autoplay:true,
-    autoplayTimeout:1500,
-    autoplayHoverPause:true,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:false,
-			dots:false
-        },
-        600:{
-            items:3,
-            nav:false
-        },
-        1000:{
-            items:5,
-            nav:false,
-            loop:false
-        }
-    }
+  loop:true,
+  margin:10,
+ autoplay:true,
+  autoplayTimeout:1500,
+  autoplayHoverPause:true,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+          nav:false,
+    dots:false
+      },
+      600:{
+          items:3,
+          nav:false
+      },
+      1000:{
+          items:5,
+          nav:false,
+          loop:false
+      }
+  }
 });
 
 
@@ -47,13 +47,13 @@ $('.owl-carousel').owlCarousel({
 
 //display feedback box
 function  fun1()
-      {
-      document.querySelector(".popup").style.display="flex";    
-      }
- //hide feedbackbox onclick cut     
-      function fun2(){
-      document.querySelector(".popup").style.display="none";    
-      }
+    {
+    document.querySelector(".popup").style.display="flex";    
+    }
+//hide feedbackbox onclick cut     
+    function fun2(){
+    document.querySelector(".popup").style.display="none";    
+    }
 
 
 /*---> Screen Preloader <---*/
@@ -61,23 +61,23 @@ function  fun1()
 var loaderx=document.getElementById('loader');
 function loadme()
 {
- loaderx.style.display='none';
+loaderx.style.display='none';
 /*setTimeout(function(){ 
-   show_log();
-   }, 5000); */
+ show_log();
+ }, 5000); */
 }
 
 /*---> Alert Box <---*/
 
-    function funalert()
-    {
-        document.querySelector(".popup").style.display="none";    
-      swal({
-    title: "Thank You For Your Feedback!",
-    text: "",
-    icon: "success",
-  });
-    }
+  function funalert()
+  {
+      document.querySelector(".popup").style.display="none";    
+    swal({
+  title: "Thank You For Your Feedback!",
+  text: "",
+  icon: "success",
+});
+  }
 
 /*---> Back To Top Button <---*/
 
@@ -86,26 +86,27 @@ const backToTop = document.querySelector('#back-to-top');
 window.addEventListener('scroll',scrollFunction);
 function scrollFunction()
 {
-  if(window.pageYOffset>300)
+
+if(window.pageYOffset>300)
+{
+  if(!backToTop.classList.contains('btnEntrance'))
   {
-    if(!backToTop.classList.contains('btnEntrance'))
-    {
-      backToTop.classList.remove('btnExit');
-      backToTop.classList.add('btnEntrance');
-      backToTop.style.display='block';
-    }
+    backToTop.classList.remove('btnExit');
+    backToTop.classList.add('btnEntrance');
+    backToTop.style.display='block';
   }
-  else
+}
+else
+{
+  if(backToTop.classList.contains('btnEntrance'))
   {
-    if(backToTop.classList.contains('btnEntrance'))
-    {
-      backToTop.classList.remove('btnEntrance');
-      backToTop.classList.add('btnExit');
-      setTimeout(function(){
-        backToTop.style.display='none';
-      },250);
-    }
+    backToTop.classList.remove('btnEntrance');
+    backToTop.classList.add('btnExit');
+    setTimeout(function(){
+      backToTop.style.display='none';
+    },250);
   }
+}
 }
 backToTop.addEventListener('click',backTop);
 function backTop()
@@ -118,19 +119,19 @@ window.scrollTo(0,0);
 
 function cancel()
 {
- document.getElementById('sign').style.display='none';
+document.getElementById('sign').style.display='none';
 }
 function show_log()
-   {
-    document.getElementById('log').style.display="block";
-   }
+ {
+  document.getElementById('log').style.display="block";
+ }
 function cancel_log()
 {
- document.getElementById('log').style.display='none';
+document.getElementById('log').style.display='none';
 }
 function show_signup()
 {
-  document.getElementById('sign').style.display='block';
+document.getElementById('sign').style.display='block';
 }
 
 
@@ -140,57 +141,55 @@ function show_signup()
 //scroll demo of nav bar
 //Demo-1
 /*var windw = this;
-
 $.fn.followTo = function ( pos ) {
-    var $this = this,
-        $window = $(windw);
-    
-    $window.scroll(function(e){
-        if ($window.scrollTop() > pos) {
-            $this.css({
-                position: 'absolute',
-                top: pos
-            });
-        } else {
-            $this.css({
-                position: 'fixed',
-                top: 0
-            });
-        }
-    });
+  var $this = this,
+      $window = $(windw);
+  
+  $window.scroll(function(e){
+      if ($window.scrollTop() > pos) {
+          $this.css({
+              position: 'absolute',
+              top: pos
+          });
+      } else {
+          $this.css({
+              position: 'fixed',
+              top: 0
+          });
+      }
+  });
 };
-
 $('#f').followTo(560);
 */
 //Demo-2 */
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-  document.getElementById('searchx').style.display='none';
+document.getElementById('searchx').style.display='none';
 var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("f").style.top = "0";
-  } else {
-    document.getElementById("f").style.top = "-100px";
-  }
-  prevScrollpos = currentScrollPos;
+if (prevScrollpos > currentScrollPos) {
+  document.getElementById("f").style.top = "0";
+} else {
+  document.getElementById("f").style.top = "-100px";
+}
+prevScrollpos = currentScrollPos;
 }
 
 
 /*---> Hide-show Serch Box <---*/
 
-  var action=1;
+var action=1;
 function search()
 {
-  if(action==1)
- {
-  document.getElementById('searchx').style.display='block';
-  action=2;
- }
- else
- {
-  document.getElementById('searchx').style.display='none';
-  action=1;
- }
+if(action==1)
+{
+document.getElementById('searchx').style.display='block';
+action=2;
+}
+else
+{
+document.getElementById('searchx').style.display='none';
+action=1;
+}
 }
 
 /*----> Auto Text Typer <----*/
@@ -202,69 +201,46 @@ $('nav').toggleClass('active')
 })
 //dynamic text
 var typed = new Typed('.text', {
-  strings: ["NITIAN","ENGINEER"],
-  typeSpeed: 120,
-  backSpeed: 120,
-  loop: true
+strings: ["NITIAN","ENGINEER"],
+typeSpeed: 120,
+backSpeed: 120,
+loop: true
 });
 //sticky navigation bar
 $(window).on('scroll',function(){
-if($(window).scrollTop())
+
+
+/*scroll indicator */
+var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+var scrolled = (winScroll / height) * 100;
+document.getElementById("myBar").style.width = scrolled + "%";
+
+
+  if($(window).scrollTop())
 {
 $('header').addClass('black');
 }
-else
+else 
 {
 $('header').removeClass('black');
 }
+
 })
+
+
 //sliding images
 var myIndex = 0;
 carousel();
 function carousel() {
-    var x = document.getElementsByClassName("mySlides");
-    var i;
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block"; 
-    setTimeout(carousel, 3000); 
+  var x = document.getElementsByClassName("mySlides");
+  var i;
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block"; 
+  setTimeout(carousel, 3000); 
 }
-
-
-/* Ripple effect */
-
-function RippleStyle(width, height, posX, posY)
-{
-  this.width=(width<=height)? height:width;
-  this.height=(width<=height)?height:width;
-  this.top= posY-(this.height * .5);
-  this.left=posX- (this.width *.5);
-
-}
-
-$(function(){
- $('.btn-rip').click(function(e){
-   var rippleE1= $('<span class="btn-ripple"></span>').appendTo(this);
-   
-   var pos = $(this).offset();
-   var width= $(this).outerwidth();
-   var height= $(this).outerHeight();
-   var posX=e.pageX - pos.left;
-   var posY = e.pageY - pos.top;
-
-   var rippleStyle=new RippleStyle(width,height,posX,posY);
-   rippleE1.css(rippleStyle);
-
- });
-
-$('.btn-rip').on('animationend webkitAnimationEnd animationend MsAnimationEnd','.btn-ripple',function(){
-  $(this).remove();
-});
-
-});
-
-
 
