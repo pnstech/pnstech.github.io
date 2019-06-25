e966e7c8946734a99ad4550e66f17d8986c3a705
Shubham Maurya <pnssoftwares7@gmail.com>
2019-06-25 21:16:25 +0530

    night

--- js/index.js
+++ js/index.js
@@ -69,9 +69,9 @@ TweenMax.staggerFrom('.svg',0.4,{opacity:0,y:100, delay:0.5},0.2/* This is durat
 
 /*QQQQQQQQQQQQQQQQQQQQQQQQ*/
 
-/*setTimeout(function(){ 
+etTimeout(function(){ 
  show_log();
- }, 5000);*/
+ }, 5000);
 }
 
 /*---> Alert Box <---*/
