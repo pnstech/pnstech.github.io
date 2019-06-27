d5b9ea9cec4ab9fcc912875698e38305a5d539a3
Shubham Maurya <pnssoftwares7@gmail.com>
2019-06-27 18:42:47 +0530

    shit

--- js/main.js
+++ js/main.js
@@ -1,24 +1,33 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-if (prevScrollpos > currentScrollPos) {
-  document.getElementById("f").style.top = "0";
-}
- else {
-  document.getElementById("f").style.top = "-100px";
-}
-prevScrollpos = currentScrollPos;
-}
 
+        //menu toggle **i dont know mhwta this shit is
+        $(document).ready(function(){
+        $('.menu-toggle').click(function(){
+        $('nav').toggleClass('active')
+        })
+        })
+    
 
-$(window).on('scroll',function(){
+        var prevScrollpos = window.pageYOffset;
+        window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+          document.getElementById("f").style.top = "0";
+        }
+         else {
+          document.getElementById("f").style.top = "-100px";
+        }
+        prevScrollpos = currentScrollPos;
+        }
         
-      if($(window).scrollTop())
-    {
-    $('header').addClass('black');
-    }
-    else 
-    {
-    $('header').removeClass('black');
-    }
-    });
+        //sticky navigation bar
+        $(window).on('scroll',function(){
+        if($(window).scrollTop())
+        {
+        $('header').addClass('black');
+        }
+        else
+        {
+        $('header').removeClass('black');
+        }
+        })
+ 
